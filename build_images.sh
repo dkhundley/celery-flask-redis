13bb0cd63ce1946c926cac303a39f266ff71ac48
David Hundley <david.k.hundley@gmail.com>
2021-02-21 20:25:00 -0600

    Updating K8s for K3s

--- build_images.sh
+++ build_images.sh
@@ -1,4 +1,10 @@
 cd containers/flask-app/
-docker build -t flask-redis .
+docker build -t flask-redis:1.0.0 .
+docker save --output ../../docker-images/flask-redis-1.0.0.tar flask-redis:1.0.0
 cd ../simple-worker/
-docker build -t redis-simple-worker .
+docker build -t redis-simple-worker:1.0.0 .
+docker save --output ../../docker-images/redis-simple-worker-1.0.0.tar redis-simple-worker:1.0.0
+cd ../../docker-images/
+sudo k3s ctr images import flask-redis-1.0.0.tar
+sudo 
+k3s ctr images import redis-simple-worker-1.0.0.tar 
